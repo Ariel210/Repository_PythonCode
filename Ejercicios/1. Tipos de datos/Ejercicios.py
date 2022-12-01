5da9219882e774cb955adf9733c0d621565be311
Ariel Izurieta <ariel_izurieta@hotmail.com>
2022-12-01 19:35:24 -0300

    ejercicio 1 tipos de datos

---
+++ Ejercicios/1. Tipos de datos/Ejercicios.py
@@ -0,0 +1,8 @@
+### Tipo De Dato Entero ###
+numero = int(input("Ingrese numero: "))
+print("Numero: ", numero, "Tipo de dato: ", type(numero))
+
+### Tipo De Dato String ###
+
+palabra = str(input("Ingrese palabra: "))
+print("Palabra: ", palabra, "Tipo de dato: ", type(palabra))
